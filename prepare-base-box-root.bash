11f737fa4998d69db05d8762cba91846fe0fbcc6
Paul Generes <paul.generes@gmail.com>
2020-07-01 17:05:54 -0500

    Lights Out!

--- prepare-base-box-root.bash
+++ prepare-base-box-root.bash
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+
 apt-get update
 
 apt-get -y remove unattended-upgrades
 
 apt-get -y upgrade
 
-apt-get -y install vim
-
-echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+apt-get -y autoremove
 
 apt-get -y install openssh-server
 
+apt-get -y install dconf-editor
+
+apt-get -y install gnome-tweaks
+
+apt-get -y install mc
--- prepare-base-box-vagrant.bash
+++ prepare-base-box-vagrant.bash
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 mkdir -p ~/.ssh \
-&& chmod 700 ~/.ssh \
-&& wget -O - https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub >> ~/.ssh/authorized_keys \
-&& chmod 644 ~/.ssh/authorized_keys
+chmod 700 ~/.ssh \
+wget -O - https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub >> ~/.ssh/authorized_keys \
+chmod 644 ~/.ssh/authorized_keys
 
+rm -f ~/.bash_history
+rm -rf ~/.local/share/mc
+
+# disable screen time outs
+gsettings set org.gnome.desktop.session idle-delay 0
+
+# remove default favorite apps
+gsettings set org.gnome.shell favorite-apps "[]"
+
+# set a black background
+gsettings set org.gnome.desktop.background picture-options 'none'
+gsettings set org.gnome.desktop.background primary-color '#000000'
+
+# remove default desktop icons
+gsettings org.gnome.shell.extensions.desktop-icons show-home false
+gsettings org.gnome.shell.extensions.desktop-icons show-trash false
