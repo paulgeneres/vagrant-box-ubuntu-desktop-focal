e5ffb055e64f78a50b7cf62372d48b367d51357d
Paul Generes <paul.generes@gmail.com>
2020-07-01 23:44:37 -0500

    Work around some sketchy behavior when there are no favorited apps

--- prepare-base-box-vagrant.bash
+++ prepare-base-box-vagrant.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir -p ~/.ssh \
-chmod 700 ~/.ssh \
-wget -O - https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub >> ~/.ssh/authorized_keys \
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+wget -O - https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub >> ~/.ssh/authorized_keys
 chmod 644 ~/.ssh/authorized_keys
 
 rm -f ~/.bash_history
@@ -11,8 +11,9 @@ rm -rf ~/.local/share/mc
 # disable screen time outs
 gsettings set org.gnome.desktop.session idle-delay 0
 
-# remove default favorite apps
-gsettings set org.gnome.shell favorite-apps "[]"
+# remove default favorite apps and add terminal app to favorites
+# the bar acts sketchy when it's completely empty (2020-07-01)
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop']"
 
 # set a black background
 gsettings set org.gnome.desktop.background picture-options 'none'
