62bbd0735bd15ea0bd8b2a5e10f999b1ffa021cf
Paul Generes <paul.generes@gmail.com>
2020-07-05 10:08:17 -0500

    I accidentally an entire word

--- prepare-base-box-vagrant.bash
+++ prepare-base-box-vagrant.bash
@@ -20,5 +20,5 @@ gsettings set org.gnome.desktop.background picture-options 'none'
 gsettings set org.gnome.desktop.background primary-color '#000000'
 
 # remove default desktop icons
-gsettings org.gnome.shell.extensions.desktop-icons show-home false
-gsettings org.gnome.shell.extensions.desktop-icons show-trash false
+gsettings set org.gnome.shell.extensions.desktop-icons show-home false
+gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
